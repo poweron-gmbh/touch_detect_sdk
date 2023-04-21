4ee159075287efc3c24b54b93c588a151d35d8b6
Gonzalo <gonzalo@poweron.one>
2023-04-21 12:23:27 +0200

    Add setup.py file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='touchdetect',
+    version='1.0',
+    description='SDK for connecting to TouchDetect',
+    author='Gonzalo Cervetti',
+    author_email='gonzalo@poweron.one',
+    packages=['touch_detect_sdk'],
+    install_requires=['bleak', 'numpy', 'future', 'pyserial']
+)
